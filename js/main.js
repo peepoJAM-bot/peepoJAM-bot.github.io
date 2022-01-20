1bf83dd4c1f76f842a58accf04418ab1087f74e0
Hzkie <90626256+peepoJAM-bot@users.noreply.github.com>
2022-01-20 18:16:17 +0200

    - updated stuff -

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 /* ============= INDEX.HTML ============= */
 
-ScrollReveal({ distance: '50px'});
+ScrollReveal({ distance: "50px" });
 
-ScrollReveal().reveal('.headline', { delay: 50, origin: 'left' });
-ScrollReveal().reveal('.tagline', { delay: 250, origin: 'left' });
-ScrollReveal().reveal('.punchline', { delay: 400, origin: 'left' });
+ScrollReveal().reveal(".headline", { delay: 50, origin: "left", cleanup: true });
+ScrollReveal().reveal(".tagline", { delay: 250, origin: "left", cleanup: true });
+ScrollReveal().reveal(".punchline", { delay: 400, origin: "left", cleanup: true });
 
-ScrollReveal().reveal('.featurestitle', { delay: 600,  origin: 'bottom' });
-ScrollReveal().reveal('.featurespunchline', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal(".featurestitle", { delay: 600, origin: "bottom", cleanup: true });
+ScrollReveal().reveal(".featurespunchline", { delay: 700, origin: "bottom", cleanup: true });
 
-ScrollReveal().reveal('.card1', { delay: 50,  origin: 'bottom' });
-ScrollReveal().reveal('.card2', { delay: 100,  origin: 'bottom' });
-ScrollReveal().reveal('.card3', { delay: 150,  origin: 'bottom' });
+ScrollReveal().reveal(".card1", { delay: 50, origin: "bottom", cleanup: true });
+ScrollReveal().reveal(".card2", { delay: 100, origin: "bottom" });
+ScrollReveal().reveal(".card3", { delay: 150, origin: "bottom" });
 
-ScrollReveal().reveal('.con-title', { delay: 200,  origin: 'left' });
-ScrollReveal().reveal('.con-sub', { delay: 300,  origin: 'left' });
-ScrollReveal().reveal('.con-started', { delay: 400,  origin: 'left' });
+ScrollReveal().reveal(".con-title", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".con-sub", { delay: 575, origin: "left" });
+ScrollReveal().reveal(".con-started", { delay: 750, origin: "left" });
 
 /* ============= COMMANDS.HTML ============= */
 
-ScrollReveal().reveal('.commands-title', { delay: 50, origin: 'left' });
-ScrollReveal().reveal('.commands-punchline', { delay: 250, origin: 'left' });
+ScrollReveal().reveal(".commands-title", { delay: 50, origin: "left" });
+ScrollReveal().reveal(".commands-punchline", { delay: 250, origin: "left" });
 
-ScrollReveal().reveal('.c-row1', { delay: 250,  origin: 'bottom' });
-ScrollReveal().reveal('.c-row2', { delay: 100,  origin: 'bottom' });
-ScrollReveal().reveal('.c-row3', { delay: 100,  origin: 'bottom' });
-ScrollReveal().reveal('.c-row4', { delay: 100,  origin: 'bottom' });
-ScrollReveal().reveal('.c-row5', { delay: 100,  origin: 'bottom' });
-ScrollReveal().reveal('.c-row6', { delay: 100,  origin: 'bottom' });
-ScrollReveal().reveal('.c-row7', { delay: 100,  origin: 'bottom' });
+ScrollReveal().reveal(".c-row1", { delay: 250, origin: "bottom" });
+ScrollReveal().reveal(".c-row2", { delay: 100, origin: "bottom" });
+ScrollReveal().reveal(".c-row3", { delay: 100, origin: "bottom" });
+ScrollReveal().reveal(".c-row4", { delay: 100, origin: "bottom" });
+ScrollReveal().reveal(".c-row5", { delay: 100, origin: "bottom" });
+ScrollReveal().reveal(".c-row6", { delay: 100, origin: "bottom" });
+ScrollReveal().reveal(".c-row7", { delay: 100, origin: "bottom" });
